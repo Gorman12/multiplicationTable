c133aa02ccbf690bb6d19c56558e0bcc24e5c29c
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-11-02 20:48:32 -0700

    add radio buttons to html and update frontend to populate multipliers array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var missedIt1 = [];
 var missedIt2 = [];
 var index;
+var numberRange;
+var multipliers = [];
 
 
 function Table(num1, num2, answer) {
@@ -9,6 +11,12 @@ function Table(num1, num2, answer) {
   this.answer = answer;
 }
 
+var numberInput = function(){
+  for(i = 0; i <= (multipliers.length - 1); i++){
+    return multipliers[i];
+  }
+};
+
 Table.prototype.multiply = function() {
   this.answer = this.num1 * this.num2;
 }
@@ -16,17 +24,17 @@ Table.prototype.multiply = function() {
 Table.prototype.newNumber = function(missedId1) {
   var i = Math.floor((Math.random() * 2) +1);
   if (missedIt1.length > 4) {
-    var point = Math.floor(Math.random() * missedId1.length);
+    var point = Math.floor(Math.random() * missedIt1.length);
     this.num1 = missedIt1[point];
     this.num2 = missedIt2[point];
     index = point;
-  } else if ((i === 2) && (missedId1 !== [])) {
-    var point = Math.floor(Math.random() * missedId1.length);
+  } else if ((i === 2) && (missedIt.length >= 1)) {
+    var point = Math.floor(Math.random() * missedIt1.length);
     this.num1 = missedIt1[point];
     this.num2 = missedIt2[point];
     index = point;
   } else {
-    this.num1 = Math.floor((Math.random()* 12) +1);
+    this.num1 = numberInput();
     this.num2 = Math.floor((Math.random()* 12) +1);
   }
 }
@@ -37,17 +45,28 @@ Table.prototype.newNumber = function(missedId1) {
 ///////////////////////////////////////////////////////////////
 
 $(document).ready(function() {
-  var table = new Table(0,0,0);
-  $(table.newNumber());
-  $("#number1").append(table.num1);
-  $("#number2").append(table.num2);
-  $("#submit").click(function(event) {
+
+  $("button#submit").click(function(event){
     event.preventDefault();
-    $(table.multiply());
+    $("input:checkbox[name=numbers]:checked").each(function(){
+      numberRange = $(this).val();
+      multipliers.push(parseInt(numberRange));
+    });
+
+    var table = new Table(0,0,0);
+
+    $("form#selectMultipliers").hide();
+    table.newNumber();
+    $("#number1").append(table.num1);
+    $("#number2").append(table.num2);
+
+
+    table.multiply();
     $(".multipliers").hide();
     $(".product").show();
     $("#answer").append(table.answer);
   });
+});
 
   $("#gotIt").click(function() {
     missedIt1.splice(index, 1);
@@ -76,4 +95,3 @@ $(document).ready(function() {
     $(".product").hide();
     $(".multipliers").show();
   });
-});
