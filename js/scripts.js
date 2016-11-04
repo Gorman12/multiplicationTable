99726e214e40f7b96995ea9cff6dbc4c7911d34d
Sean Gorman <seanedwardgorman@gmail.com>
2016-11-04 13:05:42 -0700

    I user selects missed it, then they will have to see the same question again twice and answer goit it before the question will no longer be asked. If a question is asked the first time and user selects got it, they will no longer be asked that question again. when the user answers all questions correctly (twice for previously missed it questions) the game is over.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 var missedIt1 = [];
 var missedIt2 = [];
+var numbers1 = [1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,11,11,11,11,11,11,12,12,12,12,12,12,12,12,12,12,12,12];
+var numbers2 = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,];
 var index;
+var total = 0;
+var fromNumbersArray;
+
+function random() {
+  this.num1 = Math.floor((Math.random()* 12) +1);
+  this.num2 = Math.floor((Math.random()* 12) +1);
+}
 
 
 function Table(num1, num2, answer) {
@@ -13,21 +22,26 @@ Table.prototype.multiply = function() {
   this.answer = this.num1 * this.num2;
 }
 
-Table.prototype.newNumber = function(missedId1) {
-  var i = Math.floor((Math.random() * 10) +1);
-  if (missedIt1.length > 4) {
+Table.prototype.newNumber = function(missedIt1) {
+  var i = Math.floor((Math.random() * 2) +1);
+  if (missedIt1.length > 6) {
     var point = Math.floor(Math.random() * missedIt1.length);
     this.num1 = missedIt1[point];
     this.num2 = missedIt2[point];
     index = point;
-  } else if (i < 6 && missedIt1.length >= 1) {
+    fromNumbersArray = false;
+  } else if (i < 2 && missedIt1.length >= 1) {
     var point = Math.floor(Math.random() * missedIt1.length);
     this.num1 = missedIt1[point];
     this.num2 = missedIt2[point];
     index = point;
+    fromNumbersArray = false
   } else {
-    this.num1 = Math.floor((Math.random()* 12) +1);
-    this.num2 = Math.floor((Math.random()* 12) +1);
+    var point = Math.floor(Math.random()* numbers1.length);
+    this.num1 = numbers1[point];
+    this.num2 = numbers2[point];
+    index = point
+    fromNumbersArray = true;
   }
 }
 
@@ -46,6 +60,7 @@ $(document).ready(function() {
   table.firstNumbers();
   $("#number1").append(table.num1);
   $("#number2").append(table.num2);
+
   $("#submit").click(function(event) {
     event.preventDefault();
     table.multiply();
@@ -55,21 +70,26 @@ $(document).ready(function() {
   });
 
   $("#gotIt").click(function() {
-    if (index !== 999) {
-      missedIt1.splice(index, 1);
-      missedIt2.splice(index, 1);
+    if (numbers1.length < 1) {
+      alert ("game over!")
     } else {
-
+      if (fromNumbersArray === true) {
+        numbers1.splice(index, 1);
+        numbers2.splice(index, 1);
+      } else {
+        missedIt1.splice(index, 1);
+        missedIt2.splice(index, 1);
+      }
+      $("#number1").empty();
+      $("#number2").empty();
+      $("#answer").empty();
+      table.newNumber(missedIt1);
+      $("#number1").append(table.num1);
+      $("#number2").append(table.num2);
+      $(".product").hide();
+      $(".multipliers").show();
+      alert(numbers1.length);
     }
-    index = 999;
-    $("#number1").empty();
-    $("#number2").empty();
-    $("#answer").empty();
-    table.newNumber(missedIt1);
-    $("#number1").append(table.num1);
-    $("#number2").append(table.num2);
-    $(".product").hide();
-    $(".multipliers").show();
   });
 
   $("#missedIt").click(function() {
