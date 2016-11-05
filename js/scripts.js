f2b8aba7f50459bc926471e6b48fd63129fa2c5a
Sean Gorman <seanedwardgorman@gmail.com>
2016-11-05 14:52:56 -0700

    on mobile, will Display the content for the most part as I want it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
+///////////////////variables/////////////////////
+
+
 var missedIt1 = [];
 var missedIt2 = [];
-var numbers1 = [1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11,11,11,11,11,11,11,11,11,12,12,12,12,12,12,12,12,12,12,12,12];
-var numbers2 = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12,];
+var numbers1 = [1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,7,7,7,7,7,7,7,7,7,8,8,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9];
+var numbers2 = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9];
 var index;
-var total = 0;
 var fromNumbersArray;
 
-function random() {
-  this.num1 = Math.floor((Math.random()* 12) +1);
-  this.num2 = Math.floor((Math.random()* 12) +1);
-}
+
+////////////////business logic///////////////////
 
 
 function Table(num1, num2, answer) {
@@ -22,7 +22,7 @@ Table.prototype.multiply = function() {
   this.answer = this.num1 * this.num2;
 }
 
-Table.prototype.newNumber = function(missedIt1) {
+Table.prototype.newNumber = function() {
   var i = Math.floor((Math.random() * 2) +1);
   if (missedIt1.length > 6) {
     var point = Math.floor(Math.random() * missedIt1.length);
@@ -30,7 +30,7 @@ Table.prototype.newNumber = function(missedIt1) {
     this.num2 = missedIt2[point];
     index = point;
     fromNumbersArray = false;
-  } else if (i < 2 && missedIt1.length >= 1) {
+  } else if (i !== 2 && missedIt1.length >= 1) {
     var point = Math.floor(Math.random() * missedIt1.length);
     this.num1 = missedIt1[point];
     this.num2 = missedIt2[point];
@@ -53,11 +53,11 @@ Table.prototype.firstNumbers = function() {
 }
 
 
-///////////////////////////////////////////////////////////////
-
+///////////////////User logic/////////////////////////
 
 
 $(document).ready(function() {
+  $(".gameOver").hide();
   var table = new Table(0,0,0);
   table.firstNumbers();
   $("#number1").append(table.num1);
@@ -72,8 +72,9 @@ $(document).ready(function() {
   });
 
   $("#gotIt").click(function() {
-    if (numbers1.length < 1) {
-      alert ("game over!")
+    if (numbers1.length < 2) {
+      $(".product").hide();
+      $(".gameOver").show();
     } else {
       if (fromNumbersArray === true) {
         numbers1.splice(index, 1);
@@ -85,12 +86,11 @@ $(document).ready(function() {
       $("#number1").empty();
       $("#number2").empty();
       $("#answer").empty();
-      table.newNumber(missedIt1);
+      table.newNumber();
       $("#number1").append(table.num1);
       $("#number2").append(table.num2);
       $(".product").hide();
       $(".multipliers").show();
-      alert(numbers1.length);
     }
   });
 
@@ -100,7 +100,7 @@ $(document).ready(function() {
     $("#number1").empty();
     $("#number2").empty();
     $("#answer").empty();
-    table.newNumber(missedIt1);
+    table.newNumber();
     $("#number1").append(table.num1);
     $("#number2").append(table.num2);
     $(".product").hide();
